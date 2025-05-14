0b34fe6e23b1e40291e84a5fbb555457919d18ae
Lucas Yan <lucasyanog@gmail.com>
2025-05-14 17:49:41 -0300

    feat: creating home style

--- app/page.js
+++ app/page.js
@@ -14,14 +14,14 @@ export default function Home(){
     }
 
     return (
-        <main className={`${styles.page} flex flex-col items-center justify-center min-h-screen bg-gray-100`}>
-            <section className={`${styles.box} bg-black shadow-md rounded-lg p-8 max-w-md w-full`}>
-                <h2 className={`${styles.text}`}>What is your hero name?</h2>
+        <main className={`${styles.page}`}>
+            <section className={`${styles.box}`}>
+                <h1 className={`${styles.text}`}>WHAT IS YOUR HERO NAME?</h1>
                 <form className={`${styles.form}`} onSubmit={handleSubmit}>
-                    <input type="text" name="name" placeholder="Name" className="border-2 border-gray-300 p-2 rounded-md" required />
-                    <button type="submit" className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Generate</button>
+                    <input type="text" name="name" placeholder="Name" required />
+                    <button type="submit">NEXT</button>
                 </form>
-            </section>      
+            </section>
         </main>
     );
 }
